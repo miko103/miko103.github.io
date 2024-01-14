920c5b3dbf46f70148e4facc950d63c147eea725
miko1034 <84585119+miko1034@users.noreply.github.com>
2024-01-14 21:59:47 +0000

    abc

---
+++ tribute/rockscroll.js
@@ -0,0 +1,27 @@
+const rock = [
+    "./rock/fatrock.jpg",
+    "./rock/rockNPCsmile.jpg",
+    "./rock/rockSerious.jpg",
+    "./rock/rockSerious2.jpg",
+    "./rock/rockTHEFACE1.jpg",
+    "./rock/rockWWE1.png",
+    "./rock/shockedrock.jpg",  
+    "./rock/youngrockTHEFACE.png",
+];
+
+const button = document.getElementById("button");  
+var image = document.getElementById("imageinator");
+
+  var count = 0;
+
+
+  
+
+  
+button.addEventListener("click", function(rock) {
+    image.scr = rock[count];
+    count++;
+        if (count == 9) {
+        count = 0;
+      });
+    };
