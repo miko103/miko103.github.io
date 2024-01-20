3873f4e5ba59d836783ad71399dc971c4a1c4080
miki <84585119+miko1034@users.noreply.github.com>
2024-01-20 19:44:45 +0000

    paste.js additions

--- paste/paste.js
+++ paste/paste.js
@@ -1,5 +1,36 @@
 
+const url = new URL("https://miko1034.github.com/paste?data=1");
+const params1 = new URLSearchParams(url.search);
+
+
+const currentData = url.searchParams;
+console.log(URLSearchParams.get())
+
+
 var usrinput = document.getElementById("writing");
 usrinput.onkeyup = function() {
     console.log(this.value);
 }
+
+function stringtohex(str){
+    let hex = "";
+    for (let i = 0; i < str.length; i++){
+        const charCode = str.charCodeAt(i);
+        const hexValue = charCode.toString(16);
+        hex += hexValue.padStart(2,"0")
+    }
+return hex
+}
+
+function hextostring(hex){
+    let str = "";
+    for (let i = 0; i < hex.length; i += 2){
+        const hexValue = hex.substr(i,2);
+        const decimalValue = parseInt(hexValue, 16);
+        str += String.fromCharCode(decimalValue);
+    }
+return str;
+}
+
+console.log(stringtohex("Hello"))
+console.log(hextostring("48656c6c6f"))
