79239c70d1ce1eb9c6e80bd0cca5ea560ef6be2d
miko1034 <84585119+miko1034@users.noreply.github.com>
2024-01-22 17:26:14 +0000

    changes, additions, dev to url params

--- paste/paste.js
+++ paste/paste.js
@@ -1,16 +1,18 @@
+const inputBox = document.getElementById("textBox")
 
-const url = new URL("https://miko1034.github.com/paste?data=1");
-const params1 = new URLSearchParams(url.search);
-
-
-const currentData = url.searchParams;
-console.log(URLSearchParams.get())
-
-
-var usrinput = document.getElementById("writing");
-usrinput.onkeyup = function() {
-    console.log(this.value);
+function updateText(textToWrite){
+    inputBox.setAttribute('value',textToWrite);
 }
+
+function load(){
+    const queryString = window.location.search;
+    console.log(queryString);
+    const urlParams = new URLSearchParams(queryString);
+    var dataToWrite = hextostring(urlParams.get("data"))
+    console.log(dataToWrite);
+    updateText(dataToWrite)
+    dataToWrite = stringtohex(dataToWrite)
+}//  ^from the internet 
 
 function stringtohex(str){
     let hex = "";
@@ -20,7 +22,7 @@ function stringtohex(str){
         hex += hexValue.padStart(2,"0")
     }
 return hex
-}
+} //////////////  THIS WHOLE CODE DONT EVEN WORK >:(
 
 function hextostring(hex){
     let str = "";
@@ -31,6 +33,17 @@ function hextostring(hex){
     }
 return str;
 }
+//main
+
+load()
+
+inputBox.onkeyup = function() {
+    const url = './paste.html';
+    const data = inputBox.value;
+    history.pushState({}, null, `${url}?data=${data}`);
+    load()
+  }
+
 
 console.log(stringtohex("Hello"))
 console.log(hextostring("48656c6c6f"))
