2ff6c4e6d5dd9f00a32a286e810174c22b6461c4
miko1034 <84585119+miko1034@users.noreply.github.com>
2024-01-22 11:16:04 +0000

    many tests and now works

--- test/urlParamTest/urlParamTest.js
+++ test/urlParamTest/urlParamTest.js
@@ -1,10 +1,30 @@
 const button = document.getElementById('btn');
 const inputBox = document.getElementById("inputBox")
 
+function updateText(textToWrite){
+  const text = document.getElementById("toUpdate").innerHTML = textToWrite
+}
+//initial reading from the parameter and updating text accordingly
+const queryString = window.location.search;
+console.log(queryString);
+const urlParams = new URLSearchParams(queryString);
+const dataToWrite = urlParams.get("data")
+console.log(dataToWrite);
+updateText(dataToWrite)
+//  ^from the internet 
 
-
-button.addEventListener('click', () => {
+//now it just does stuff and things
+inputBox.onkeyup = function() {
   const url = './urlParamTest.html';
-  const data = inputBox.value
-  window.location.href = `${url}?data=${data}`;
-});
+  const data = inputBox.value;
+  //window.location.href = `${url}?data=${data}`;
+  history.pushState({}, null, `${url}?data=${data}`);
+  //experimental ahead
+  const queryString = window.location.search;
+  console.log(queryString);
+  const urlParams = new URLSearchParams(queryString);
+  const dataToWrite = urlParams.get("data")
+  console.log(dataToWrite);
+  updateText(dataToWrite)
+}
+//messy but it works so i wont fix it
